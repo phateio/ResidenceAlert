230df3567c04b996df1202972594c1b6f7f581b8
WhiteCat <whitecatx6@gmail.com>
2019-12-31 17:57:20 +0800

    Add new placerholder onClicked

--- src/main/java/io/github/phateio/residencealert/ResidenceAlert.java
+++ src/main/java/io/github/phateio/residencealert/ResidenceAlert.java
@@ -34,15 +34,15 @@ public class ResidenceAlert extends JavaPlugin implements Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onBlockPlace(BlockPlaceEvent event) {
-        alertCheck(event.getPlayer(), event.getBlockPlaced().getType(), event.getBlock().getLocation());
+        alertCheck(event.getPlayer(), event.getBlock().getType(), event.getBlockPlaced().getType(), event.getBlock().getLocation());
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onBucketPlace(PlayerBucketEmptyEvent event) {
-        alertCheck(event.getPlayer(), event.getBucket(), event.getBlock().getLocation());
+        alertCheck(event.getPlayer(), event.getBlockClicked().getType(), event.getBucket(), event.getBlock().getLocation());
     }
 
-    private void alertCheck(Player player, Material type, Location loc) {
+    private void alertCheck(Player player, Material clicked, Material type, Location loc) {
         if (loc.getBlockY() < ignoreYBelow) return;
         if (disabledWorlds.contains(loc.getWorld().getName())) return;
         if (!alertBlocks.contains(type)) return;
@@ -57,6 +57,7 @@ public class ResidenceAlert extends JavaPlugin implements Listener {
 
         final String output = alertMessage
                 .replace("${Block}", type.name())
+                .replace("${onClicked}", clicked.name())
                 .replace("${Player}", player.getName())
                 .replace("${x}", loc.getBlockX() + "")
                 .replace("${y}", loc.getBlockY() + "")
